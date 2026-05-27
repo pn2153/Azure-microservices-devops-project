87fb621a2ac52c47fa78c4a17469407a466f349f
Niyati <niyati.phanse8@gmail.com>
2026-05-27 12:33:08 +0530

    added dev env rg and bootstrap rg and storage

---
+++ infra/environments/dev/acr.tf
@@ -0,0 +1,12 @@
+resource "azurerm_container_registry" "main" {
+
+  name                = "acrmicroservicesdev123"
+
+  resource_group_name = azurerm_resource_group.main.name
+
+  location            = azurerm_resource_group.main.location
+
+  sku                 = "Basic"
+
+  admin_enabled       = true
+}
---
+++ infra/environments/dev/terraform.tfvars
@@ -0,0 +1,3 @@
+resource_group_name = "rg-app-dev-niyati"
+
+location = "Central India"
